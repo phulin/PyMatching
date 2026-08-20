38a1b3fbb666b0b28b0016e8a1f5ecbfde70356a
Patrick Hulin <patrick.hulin@gmail.com>
2026-08-20 13:48:38 -0400

    Register one-pass decode-to-edges-and-weight API

--- src/pymatching/pymatching.pybind.cc
+++ src/pymatching/pymatching.pybind.cc
@@ -14,6 +14,7 @@
 
 #include "pybind11/pybind11.h"
 #include "pymatching/rand/rand_gen.pybind.h"
+#include "pymatching/sparse_blossom/driver/fast_decode.pybind.h"
 #include "pymatching/sparse_blossom/driver/namespaced_main.h"
 #include "pymatching/sparse_blossom/driver/user_graph.pybind.h"
 
@@ -33,6 +34,7 @@ PYBIND11_MODULE(_cpp_pymatching, m) {
     auto matching_graph = pm_pybind::pybind_user_graph(m);
     pm_pybind::pybind_user_graph_methods(m, matching_graph);
     pm_pybind::pybind_rand_gen_methods(m);
+    pm_pybind_fast::register_fast_decode(m);
     m.def("main", &pymatching_main, pybind11::kw_only(), pybind11::arg("command_line_args"), R"pbdoc(
 Runs the command line tool version of pymatching with the given arguments.
 )pbdoc");
